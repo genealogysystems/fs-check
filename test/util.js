c1376b1b90857c6e274b9ee139d498c6e616117d
John Clark <socrates37@gmail.com>
2014-05-21 07:24:29 -0600

    added coverage for util.markdown

--- test/util.js
+++ test/util.js
@@ -147,4 +147,29 @@ describe('util', function(){
 
   });
 
+  describe('#markdown()', function(){
+    
+    it('should return html', function() {
+      var html = util.markdown(function(){/*
+        # h1
+        ## h2
+        ### h3
+        Stuff!
+      */});
+
+      expect(html).to.equal("<h1>h1</h1>\n<h2>h2</h2>\n<h3>h3</h3>\n<p>Stuff!</p>\n");
+    });
+
+    it('should replace mustache vars', function() {
+      var html = util.markdown(function(){/*
+        number {{one}}
+        number {{two}}
+        missing {{three}}
+      */},{one:'one', two: 'two'});
+
+      expect(html).to.equal("<p>number one\nnumber two\nmissing </p>\n");
+    });
+
+  });
+
 });
