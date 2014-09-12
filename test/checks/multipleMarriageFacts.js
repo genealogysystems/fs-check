c5caa875efbc8c9bae30cf5562f5f7fb95268e0d
Justin York <justin@genealogysystems.com>
2014-09-12 16:33:57 +0000

    Enhance description of multiple marriage facts; closes #54

--- test/checks/multipleMarriageFacts.js
+++ test/checks/multipleMarriageFacts.js
@@ -90,31 +90,4 @@ describe('multipleMarriageFacts', function(){
     expect(opportunity.person).to.equal(wife);
   });
 
-  it('should use husband when wife is missing', function() {
-    var husband = new FamilySearch.Person({}),
-        wife = undefined,
-        marriage = new FamilySearch.Couple({
-          husband: husband,
-          wife: wife,
-          facts: [
-            new FamilySearch.Fact({
-              type: 'http://gedcomx.org/Marriage',
-              date: 'January 1, 1900',
-              formalDate: '+1900-01-01',
-              place: 'Provo, Utah, United States of America'
-            }),
-            new FamilySearch.Fact({
-              type: 'http://gedcomx.org/Marriage',
-              date: 'January 1, 1900',
-              formalDate: '+1900-01-01',
-              place: 'Provo, Utah, United States of America'
-            })
-          ]
-        });
-
-    var opportunity = fsCheck.check(wife, husband, marriage);
-    utils.validateSchema(fsCheck, opportunity);
-    expect(opportunity.person).to.equal(husband);
-  });
-
 });
