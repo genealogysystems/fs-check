56e85781118c91e1f168edc61206abc59df36d43
Justin York <justin@genealogysystems.com>
2014-09-12 15:58:30 +0000

    Forgot files; #60

---
+++ test/checks/standardizeMarriageDate.js
@@ -0,0 +1,158 @@
+var libPath = process.env.TEST_COV ? '../../lib-cov' : '../../lib',
+    path = require('path'),
+    expect = require('chai').expect,
+    FamilySearch = require('../../vendor/familysearch-javascript-sdk.js'),
+    fsCheck = require(path.join(libPath, 'index.js')).id('standardizeMarriageDate'),
+    utils = require('../test-utils.js'),
+    doc = require('../../docs/util.js');
+
+describe('standardizeMarriageDate', function(){
+
+  it('should return nothing when there is no marriage facts', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: []
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+  it('should return nothing when there is multiple marriage facts', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01',
+              place: 'Provo, Utah, United States of America'
+            }),
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+  it('should return nothing when there is no original date', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+  it('should return nothing when there is a formal date', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+    marriage.facts[0].date.normalized = [{ value: '1 January 1900' }];
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+  it('should return an opportunity when there is a original but no formal date', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+        
+    marriage.id = 'MMMM-MMM';
+    husband.display = { name: 'Elmer Fudd' };
+    wife.display = { name: 'Thelma Lousie' };
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    doc('standardizeMarriageDate', opportunity);
+    utils.validateSchema(fsCheck, opportunity);
+    expect(opportunity.person).to.equal(wife);
+  });
+
+  it('should move opportunity to husband when wife is missing', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = undefined,
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+    utils.validateSchema(fsCheck, opportunity);
+    expect(opportunity.person).to.equal(husband);
+  });
+
+  it('should return nothing when husband and wife are undefined', function() {
+    var husband = undefined,
+        wife = undefined,
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+    expect(opportunity).to.equal(undefined);
+  });
+
+});
---
+++ test/checks/standardizeMarriagePlace.js
@@ -0,0 +1,162 @@
+var libPath = process.env.TEST_COV ? '../../lib-cov' : '../../lib',
+    path = require('path'),
+    expect = require('chai').expect,
+    FamilySearch = require('../../vendor/familysearch-javascript-sdk.js'),
+    fsCheck = require(path.join(libPath, 'index.js')).id('standardizeMarriagePlace'),
+    utils = require('../test-utils.js'),
+    doc = require('../../docs/util.js');
+
+describe('standardizeMarriagePlace', function(){
+
+  it('should return nothing when there is no marriage facts', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: []
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+  it('should return nothing when there is multiple marriage facts', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01',
+              place: 'Provo, Utah, United States of America'
+            }),
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+  it('should return nothing when there is no original place', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+  it('should return nothing when there is a normalized place', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01',
+              place: 'Provo, Utah, United States of America',
+              normalizedPlace: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+  it('should return an opportunity when there is a original but no normalized place', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = new FamilySearch.Person({}),
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+        
+    marriage.id = 'MMMM-MMM';
+    husband.display = { name: 'Elmer Fudd' };
+    wife.display = { name: 'Thelma Lousie' };
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    doc('standardizeMarriagePlace', opportunity);
+    utils.validateSchema(fsCheck, opportunity);
+    expect(opportunity.person).to.equal(wife);
+  });
+
+  it('should move opportunity to husband when wife is missing', function() {
+    var husband = new FamilySearch.Person({}),
+        wife = undefined,
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+    utils.validateSchema(fsCheck, opportunity);
+    expect(opportunity.person).to.equal(husband);
+  });
+
+  it('should return nothing when husband and wife are undefined', function() {
+    var husband = undefined,
+        wife = undefined,
+        marriage = new FamilySearch.Couple({
+          husband: husband,
+          wife: wife,
+          facts: [
+            new FamilySearch.Fact({
+              type: 'http://gedcomx.org/Marriage',
+              date: 'January 1, 1900',
+              formalDate: '+1900-01-01',
+              place: 'Provo, Utah, United States of America'
+            })
+          ]
+        });
+
+    var opportunity = fsCheck.check(wife, husband, marriage);
+
+    expect(opportunity).to.equal(undefined);
+  });
+
+});
