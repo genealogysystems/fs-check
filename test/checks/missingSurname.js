f463f4c49be314d326f28bebcb7976738f838223
Justin <justincyork@gmail.com>
2015-03-17 16:42:50 +0000

    missingSurname

--- test/checks/missingSurname.js
+++ test/checks/missingSurname.js
@@ -1,24 +1,22 @@
-var libPath = process.env.TEST_COV ? '../../lib-cov' : '../../lib',
-    path = require('path'),
-    expect = require('chai').expect,
-    FamilySearch = require('../../vendor/familysearch-javascript-sdk.js'),
-    fsCheck = require(path.join(libPath, 'index.js')).id('missingSurname'),
+var expect = require('chai').expect,
+    fsCheck = require('../../lib/index.js').id('missingSurname'),
+    doc = require('../../docs/util.js'),
     utils = require('../test-utils.js'),
-    doc = require('../../docs/util.js');
+    FS = utils.FS;
 
-describe.skip('missingSurname', function(){
+describe('missingSurname', function(){
 
   it('should return nothing when there is no name', function(){
-    var opportunity = fsCheck.check(new FamilySearch.Person());
+    var opportunity = fsCheck.check(FS.createPerson());
     expect(opportunity).to.equal(undefined);
   });
 
   it('should return nothing when there is a surname', function(){
-    var person = new FamilySearch.Person({
+    var person = FS.createPerson({
       gender: 'http://gedcomx.org/Female',
       names: [
-        new FamilySearch.Name({
-          surname: 'Adams'
+        FS.createName({
+          $surname: 'Adams'
         })
       ]
     });
@@ -27,11 +25,11 @@ describe.skip('missingSurname', function(){
   });
   
   it('should return an opportunity when there is a given name with no surname', function(){
-    var opportunity = fsCheck.check(new FamilySearch.Person({
+    var opportunity = fsCheck.check(FS.createPerson({
       gender: 'http://gedcomx.org/Female',
       names: [
-        new FamilySearch.Name({
-          givenName: 'Mary'
+        FS.createName({
+          $givenName: 'Mary'
         })
       ]
     }));
