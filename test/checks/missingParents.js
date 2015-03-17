694c9fed4b234776c1754107c2f7384aaf530244
Justin <justincyork@gmail.com>
2015-03-17 16:38:17 +0000

    missingParents

--- test/checks/missingParents.js
+++ test/checks/missingParents.js
@@ -1,34 +1,32 @@
-var libPath = process.env.TEST_COV ? '../../lib-cov' : '../../lib',
-    path = require('path'),
-    expect = require('chai').expect,
-    FamilySearch = require('../../vendor/familysearch-javascript-sdk.js'),
-    fsCheck = require(path.join(libPath, 'index.js')).id('missingParents'),
+var expect = require('chai').expect,
+    fsCheck = require('../../lib/index.js').id('missingParents'),
+    doc = require('../../docs/util.js'),
     utils = require('../test-utils.js'),
-    doc = require('../../docs/util.js');
+    FS = utils.FS;
 
-describe.skip('missingParents', function(){
+describe('missingParents', function(){
 
   it('should return nothing when there is a parent', function() {
-    var child = new FamilySearch.Person({}),
-        parent = new FamilySearch.Person({}),
+    var child = FS.createPerson({}),
+        parent = FS.createPerson({}),
         opportunity = fsCheck.check(child, [parent]);
     expect(opportunity).to.equal(undefined);
   });
   
   it('should return an opportunity when there are no parents', function() {
-    var child = new FamilySearch.Person({
+    var child = FS.createPerson({
           names: [
-            new FamilySearch.Name({
-              givenName: 'Bob',
-              surname: 'Freemer'
+            FS.createName({
+              $givenName: 'Bob',
+              $surname: 'Freemer'
             })
           ],
           facts: [
-            new FamilySearch.Fact({
+            FS.createFact({
               type: 'http://gedcomx.org/Birth',
-              date: 'January 1, 1900',
-              formalDate: '+1900-01-01',
-              place: 'Provo, Utah, United States of America'
+              $date: 'January 1, 1900',
+              $formalDate: '+1900-01-01',
+              $place: 'Provo, Utah, United States of America'
             })
           ]
         });
@@ -40,10 +38,6 @@ describe.skip('missingParents', function(){
     
     doc('missingParents', opportunity);
     utils.validateSchema(fsCheck, opportunity, true);
-    expect(opportunity.findarecord.tags).to.deep.equal(['birth']);
-    expect(opportunity.findarecord.from).to.equal(1897);
-    expect(opportunity.findarecord.to).to.equal(1903);
-    expect(opportunity.findarecord.place).to.equal('Provo, Utah, United States of America');
   });
   
 });
