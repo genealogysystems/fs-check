abd8bb6ad4a9ef486a7d4c80067c4260cc773fbb
Justin <justincyork@gmail.com>
2015-05-19 21:17:54 +0000

    better parent relationship type logic; fixes #74

--- test/checks/multipleParents.js
+++ test/checks/multipleParents.js
@@ -40,8 +40,8 @@ describe('multipleParents', function(){
     var relationships = {
       getParentRelationships: function() {
         return [
-          createParentRelationship('StepParent', 'BiologicalParent'),
-          createParentRelationship('', 'StepParent')
+          createParentRelationship('FATHER1', 'StepParent', 'MOTHER1', 'BiologicalParent'),
+          createParentRelationship('FATHER2', 'BiologicalParent', 'MOTHER2', 'StepParent')
         ];
       }
     };
@@ -50,7 +50,7 @@ describe('multipleParents', function(){
     expect(opportunity).to.not.exist;
   });
 
-  it('should return an opportunity when there is more than one biological parent relationship', function() {
+  it('should return an opportunity when there is more than TWO biological parent relationship', function() {
     var person = FS.createPerson();
     person.id = 'XXX-123';
     person.display = { name: 'Sue Adams' };
@@ -58,8 +58,8 @@ describe('multipleParents', function(){
     var relationships = {
       getParentRelationships: function() {
         return [
-          createParentRelationship('', 'BiologicalParent'),
-          createParentRelationship('StepParent', 'BiologicalParent')
+          createParentRelationship('FATHER1', 'BiologicalParent', 'MOTHER1', 'BiologicalParent'),
+          createParentRelationship('FATHER2', 'StepParent', 'MOTHER2', 'BiologicalParent')
         ];
       }
     };
@@ -69,24 +69,48 @@ describe('multipleParents', function(){
     doc('multipleParents', opportunity);
     utils.validateSchema(fsCheck, opportunity);
   });
+  
+  it('should not count the same parent twice', function(){
+    var person = FS.createPerson();
+    person.id = 'XXX-123';
+    person.display = { name: 'Sue Adams' };
+
+    var relationships = {
+      getParentRelationships: function() {
+        return [
+          createParentRelationship('FATHER', 'BiologicalParent', 'MOTHER1', 'BiologicalParent'),
+          createParentRelationship('FATHER', 'BiologicalParent', 'MOTHER2', 'StepParent')
+        ];
+      }
+    };
+
+    var opportunity = fsCheck.check(person, relationships, {});
+    expect(opportunity).to.not.exist;
+  })
 
 });
 
-function createParentRelationship(fatherType, motherType){
+function createParentRelationship(fatherId, fatherType, motherId, motherType){
   return {
+    $getFatherId: function(){
+      return fatherId;
+    },
     $getFatherFacts: function(){
       return fatherType ? [
         FS.createFact({
           type: 'http://gedcomx.org/' + fatherType
         })
-      ] : [];
+      ] : undefined;
+    },
+    $getMotherId: function(){
+      return motherId;
     },
     $getMotherFacts: function(){
       return motherType ? [
         FS.createFact({
           type: 'http://gedcomx.org/' + motherType
         })
-      ] : [];
+      ] : undefined;
     }
   };
 };
